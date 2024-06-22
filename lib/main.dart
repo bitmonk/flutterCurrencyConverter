34db1e2575d31542af616f108451e77281241ce6
Parbat Tamang <parbattamang1234@gmail.com>
2024-06-22 16:28:44 +0545

    center

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main(){
-runApp(const MyApp());
-
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget{
@@ -11,7 +10,11 @@ class MyApp extends StatelessWidget{
   @override
    Widget build(BuildContext context){
     return MaterialApp(
-      home: Text('Hello World !!'),
+      home: Scaffold(
+        body: Center(child: 
+         Text('Hello World !!'),
+        ), 
+      ),
     );
     }
 }
